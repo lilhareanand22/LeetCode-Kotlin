3196f4b145e673c5169e7e027f1759e4ef233344
lilhareanand22 <lilhareanand22@gmail.com>
2026-09-06 16:52:26 +0530

    added anagram

--- src/main/kotlin/excercise/Anagram.kt
+++ src/main/kotlin/excercise/Anagram.kt
@@ -1,4 +1,6 @@
-package arrays.hashing
+package excercise
+
+
 
 fun main() {
     val s = "anagram"
@@ -8,24 +10,22 @@ fun main() {
 }
 
 private fun isAnagram(s:String, t:String) : Boolean {
-    val map = hashMapOf<Char, Int>()
-
+    val map = mutableMapOf<Char, Int>()
     if(s.length != t.length) {
         return false
     }
-
-    for (c in s) {
-        map [c] = map.getOrDefault(c,0) + 1
-    }
-
-    for (ch in t) {
+   for(ch in s) {
+       map[ch]= map.getOrDefault(ch, 0) + 1
+   }
+   for(ch  in t) {
        if(!map.containsKey(ch)) {
            return false
        }
-       map[ch] = map[ch]!! - 1
+       map[ch] = map[ch]!! -1
        if(map[ch]!! < 0) {
            return false
        }
-    }
+   }
     return true
+
 }
